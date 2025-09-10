04b06f7380c4ebb09c4dd1771954880d638c944d
Clemente Velasco <clemente.velasco@gmail.com>
2025-09-10 10:07:28 -0400

    ENH: Remove dcm2niix conversion
    Removing dcm2niix from dicom conversion script. Functionality will be moved to a separate script

--- src/dicom_conversion.py
+++ src/dicom_conversion.py
@@ -48,6 +48,7 @@ def main():
 #        patient_id ="Unknow_ID"
 
     # ds_modality
+    Series_Description = ds[0x0008, 0x103e].value
     if (0x0018, 0x1030) in ds:
         protocol_name = ds[0x0018, 0x1030].value
         ds_modality = protocol_name
@@ -55,7 +56,6 @@ def main():
         acquisition_contrast = ds[0x0008, 0x9209].value
         ds_modality = acquisition_contrast
     elif (0x0008, 0x103e) in ds:
-        Series_Description = ds[0x0008, 0x103e].value
         ds_modality = Series_Description
     else:
         ds_modality = None
@@ -85,24 +85,27 @@ def main():
     patient_id = args.subjectid
     visit_id = args.visitid
     print("Subject ID           : ", patient_id)
-    print("Visit ID           : ", visit_id)
+    print("Visit ID             : ", visit_id)
+    print("Series Description   : ", Series_Description)
 #    subject_id = f"sub-{patient_id}_{acquisition_date}"
     subject_id = f"{patient_id}"
     s_number=f"run_{serie_number}"
 
     # SET SEQUENCES MATCHES
     diffusion_matches = ["dti", "DIFFUSION", "DIFF", "diff", "dwi", "DTI", "EPI_highres_with_distortion", "epi_me_128", "dual"]
-    derivative_matches = ["_FA", "_ColFA", "_ADC", "_TENSOR_B0", "_TENSOR", "_TRACEW"]
-    fieldmap_matches = ["fieldmap"]
+    derivative_matches = ["_FA", "_ColFA", "_ADC", "_TENSOR_B0", "_TENSOR", "_TRACEW", "_DFC"]
+    fieldmap_matches = ["fieldmap", "field_map"]
     T2W_matches = ["HASTE", "haste", "T2MATCH", "AX_T2_FS"]
     T1W_matches = ["VIBE", "vibe"]
     fmri_matches = ["fmri", "fMRI", "FMRI"]
     Multi_EchoTime=["_me", "echo", "cho" ]
 
     # CHECK FOR SEQUENCE MATCHES
+    foldername = os.path.basename(args.input_directory) # also check for matches with folder name
     mri_modality="unknown"
     seq_name="other"
-    if any([x in ds_modality for x in diffusion_matches]) and not any([y in ds_modality for y in derivative_matches]):
+    # This if statement is supposed to filter out matches for derivative diffusion matches based on the protocol name or series description
+    if any([x in ds_modality for x in diffusion_matches]) and not any([y in ds_modality for y in derivative_matches]) and not any([z in Series_Description for z in derivative_matches]):
         if any([x in ds_modality for x in Multi_EchoTime]):
             mri_modality = "dwi_me"
             seq_name = "dwi_me"
@@ -125,12 +128,10 @@ def main():
     fullid = f"{subject_id}_{visit_id}_{seq_name}_{s_number}"
 
 
-    output_directory_mrconvert = os.path.join(args.output_directory, mri_modality, s_number, "mrconvert")
-    output_directory_dcm2niix = os.path.join(args.output_directory, mri_modality, s_number, "dcm2niix")
+    output_directory_mrconvert = os.path.join(args.output_directory, mri_modality, s_number)
 
     tmp_directory = os.path.join(output_directory_mrconvert, "tmp")
     os.makedirs(output_directory_mrconvert, exist_ok=True)
-    os.makedirs(output_directory_dcm2niix, exist_ok=True)
     os.makedirs(tmp_directory, exist_ok=True)
 
 
@@ -145,19 +146,13 @@ def main():
     for file in os.listdir(args.input_directory):
         filename = os.fsdecode(file)
         input_file = os.path.join(args.input_directory, filename)
-        base_name = os.path.splitext(filename)[0]
+        #base_name = os.path.splitext(filename)[0]
+        base_name = os.path.basename(filename)
         new_file = os.path.join(tmp_directory, f'{base_name}_jpeg')
 
         # Perform the conversion using dcmdjpeg
         subprocess.run(['dcmdjpeg', input_file, new_file])
 
-
-    # Set dcm2niix convert command
-    # dcm2niix_outputname=f'{subject_id}_{mri_modality}_run-%s_desc-%p_echo-%e'
-    dcm2niix_outputname=f'{subject_id}_{mri_modality}'
-    cmd_dcm2niix = [
-    'dcm2niix', '-z', 'o' , '-w', '0', '-v', '0', dcm2niix_outputname, '-f', fullid, '.nii.gz', '-o', output_directory_dcm2niix, args.input_directory
-    ]
 
     if mri_modality.startswith('dwi'):
         print("MRI Modality is :", mri_modality)
@@ -174,18 +169,17 @@ def main():
         # adding '-export_pe_table', pe_table, '-export_pe_eddy', pe_eddy, doesn't work for some data
         cmd_mrconvert = [
         'mrconvert', '-clear_property', 'comments', '-json_export', json_export, '-export_grad_mrtrix', grad_mrtrix, '-export_grad_fsl',
-        grad_fsl_bvecs, grad_fsl_bvals, tmp_directory, os.path.join(output_directory_mrconvert, f'{fullid}.nii.gz')#, '-force'
+        grad_fsl_bvecs, grad_fsl_bvals, args.input_directory, os.path.join(output_directory_mrconvert, f'{fullid}.nii.gz')#, '-force'
         ]
+#        'mrconvert', '-clear_property', 'comments', '-json_export', json_export, '-export_grad_mrtrix', grad_mrtrix, '-export_grad_fsl',
+#        grad_fsl_bvecs, grad_fsl_bvals, tmp_directory, os.path.join(output_directory_mrconvert, f'{fullid}.nii.gz')#, '-force'
+#        ]
 
         print(' '.join(cmd_mrconvert))  # Print the command
         subprocess.run(cmd_mrconvert)
-        print(' '.join(cmd_dcm2niix))
-        subprocess.run(cmd_dcm2niix)
-
 
         # Goal is creating the following
         json_file_params = os.path.join(output_directory_mrconvert, f'{fullid}_dMRI_params.json')
-        acq_file_dcm2niix = os.path.join(output_directory_dcm2niix, f'{fullid}_acqparams_dcm2niix.txt')
         acq_file_mrconvert = os.path.join(output_directory_mrconvert, f'{fullid}_acqparams_mrconvert.txt')
 
 
@@ -228,52 +222,6 @@ def main():
             }
 
 
-            # Write the specific information to the JSON file
-            # These aren't used for anything. pydicom header stores PHI
-            #with open(json_file_params, 'w') as json_file:
-            #    json.dump(specific_info, json_file)
-
-            #with open(pydicom_header, 'w') as f:
-            #        f.write(str(ds))
-
-
-            ###############
-            # acpparams.txt for topup and eddy
-
-            # assuming 4 echoes
-            echoes = [1 ,2, 3, 4]
-
-            for echo in echoes:
-                json_file = subprocess.check_output(
-                    f'find "{output_directory_dcm2niix}" -type f -name "*echo-{echo}*.json"',
-                    shell=True, text=True).strip()
-                
-                if not json_file:
-                    print(f"No json file found for echo {echo}, skipping")
-                    continue
-
-                with open(json_file, "r") as f:
-                    json_data = json.load(f)
-                
-                phase_dir = json_data["PhaseEncodingDirection"]
-                readout_time = json_data["TotalReadoutTime"]
-                direction = pow(-1, int(echo) - 1)
-                
-
-                if "j" in phase_dir:
-                    with open(acq_file_dcm2niix, "a") as acq_file:
-                        acq_file.write(f"0 {direction} 0 {readout_time}\n")
-                elif "i" in phase_dir:
-                    with open(acq_file_dcm2niix, "a") as acq_file:
-                        acq_file.write(f"{direction} 0 0 {readout_time}\n")
-                elif "k" in phase_dir:
-                    with open(acq_file_dcm2niix, "a") as acq_file:
-                        acq_file.write(f"0 0 {direction} {readout_time}\n")
-                else:
-                    print("Phase encoding direction not recognized")
-                    exit(1)
-
-
     else:
         # Convert other modalities without gradient information
         cmd = [
@@ -287,11 +235,6 @@ def main():
 
     # Remove dmcdjpeg temporary directory
     subprocess.run(['rm', '-rf', tmp_directory])
-
-    # Run dcm2niix for non-DWI
-    print(' '.join(cmd_dcm2niix))
-    subprocess.run(cmd_dcm2niix)
-
 
 print(' ')
         
